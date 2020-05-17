51341d46950f429362b59ad1e313fc5e6ee608c2
Rajdeepc <rajrock38@gmail.com>
2020-05-17 17:15:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Colorful BoxShadow
+
+A Simple npm package to make your images or elements have colorful box shadows.
+
+## Installation
+
+```
+npm i colorBoxShadow --save
+```
+
+## How to use
+
+```
+import { colorBoxShadow } from 'colorBoxShadow';
+
+colorBoxShadow({
+    shadow_type: 'light-1',
+    shadow_color: '#fafafa'
+});
+```
+
+## Options
+
+colorBoxShadow supports two options, both of which are optional
+
+* *shadow_type* - light-1 | light-2 | medium-1 | medium-2 | hard | Defaults to light-1
+* *shadow_color* - defaults to #fafafa
+
+## Shadow Types:
+
+light-1:
+
+['light-1'](light1.png)
+
+light-2:
+
+['light-2'](light2.png)
+
+medium-1:
+
+['medium-1'](medium1.png)
+
+medium-2:
+
+['medium-2'](medium2.png)
+
+hard:
+
+['hard'](hard.png)
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const hexToRGBA = (hex, opacity) => {
   return (
     "rgba(" +
-    (hex = hex.replace("#", ""))
-      .match(new RegExp("(.{" + hex.length / 3 + "})", "g"))
+    ((hex = hex.replace("#", ""))
+      .match(new RegExp("(.{" + hex.length / 3 + "})", "g")) || '#fafafa' )
       .map(function (l) {
         return parseInt(hex.length % 2 ? l + l : l, 16);
       })
-      .concat(opacity || 1)
+      .concat(opacity || 0.22)
       .join(",") +
     ")"
   );
@@ -20,19 +20,19 @@ const colorShadow = (options) => {
 
   switch (options.shadow_type) {
     case "light-1":
-      options.shadow_type = `0 1px 3px ${hexToRGBA(options.color,0.12)},0 1px 2px ${hexToRGBA(options.color,0.24)}`;
+      options.shadow_type = `0 1px 3px ${hexToRGBA(options.shadow_color,0.12)},0 1px 2px ${hexToRGBA(options.shadow_color,0.24)}`;
       break;
     case "light-2":
-      options.shadow_type = `0 3px 6px ${hexToRGBA(options.color,0.16)},0 3px 6px ${hexToRGBA(options.color,0.23)}`;
+      options.shadow_type = `0 3px 6px ${hexToRGBA(options.shadow_color,0.16)},0 3px 6px ${hexToRGBA(options.shadow_color,0.23)}`;
       break;
     case "medium-1":
-      options.shadow_type = `0 10px 20px ${hexToRGBA(options.color,0.19)},0 3px 6px ${hexToRGBA(options.color,0.23)}`;
+      options.shadow_type = `0 10px 20px ${hexToRGBA(options.shadow_color,0.19)},0 3px 6px ${hexToRGBA(options.shadow_color,0.23)}`;
       break;
     case "medium-2":
-      options.shadow_type = `0 14px 28px ${hexToRGBA(options.color,0.25)},0 3px 6px ${hexToRGBA(options.color,0.22)}`;
+      options.shadow_type = `0 14px 28px ${hexToRGBA(options.shadow_color,0.25)},0 3px 6px ${hexToRGBA(options.shadow_color,0.22)}`;
       break;
-    case "hard-1":
-      options.shadow_type = `0 19px 38px ${hexToRGBA(options.color,0.30)},0 3px 6px ${hexToRGBA(options.color,0.22)}`;
+    case "hard":
+      options.shadow_type = `0 19px 38px ${hexToRGBA(options.shadow_color,0.30)},0 3px 6px ${hexToRGBA(options.shadow_color,0.22)}`;
       break;
     default:
         options.shadow_type = `0 1px 3px ${hexToRGBA(options.color,0.12)},0 1px 2px ${hexToRGBA(options.color,0.24)}`;
@@ -43,4 +43,4 @@ const colorShadow = (options) => {
   });
 };
 
-module.exports.colorShadow = colorShadow;
+module.exports.colorBoxShadow = colorBoxShadow;
